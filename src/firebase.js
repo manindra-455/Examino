e6b7153d51532884ec473fe27e916fe663276cc5
Manindra <manindragupta009@gmail.com>
2025-06-12 18:02:22 +0530

    final commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCQM68pitvY4tLfnplEdfIpV0gtF6Op4bE",
-  authDomain: "examino-f22a4.firebaseapp.com",
-  projectId: "examino-f22a4",
-  storageBucket: "examino-f22a4.firebasestorage.app",
-  messagingSenderId: "440994412722",
-  appId: "1:440994412722:web:c035b90394166b0a9af1ab",
-  measurementId: "G-5S1X3XP235"
+
 };
 
 // Initialize Firebase
