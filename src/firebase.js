46f3b59594d1696b9f86c984cd1091eff47e8ca9
Manindra Nilesh <123898666+manindra-455@users.noreply.github.com>
2026-01-01 16:01:56 +0530

    Update firebaseConfig with environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"; 
 
 const firebaseConfig = {
-
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
